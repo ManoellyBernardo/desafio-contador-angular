eeb9c7f4455555d98a8de6a135ef9aca760745ff
ManoellyBernardo <manoellymaria@gmail.com>
2022-11-13 15:40:24 -0300

    Desafio contador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'desafio-contador';
+
+  cont:number = 0
+
+  contarPositivo():number{
+    return this.cont++
+  }
+  
+  contarNegativo():number{
+    return this.cont--
+  }
 }
